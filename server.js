dd072698ca0c16a25594734d89ba176594591827
singhdilraj016-sys <example@gmail.com>
2026-05-17 21:17:07 +0530

    fixed render crash

--- server.js
+++ server.js
@@ -17,12 +17,8 @@ app.use(express.json());
 /* ===================================== */
 
 mongoose.connect(process.env.MONGO_URL)
-.then(() => {
-    console.log("MongoDB Connected");
-})
-.catch((err) => {
-    console.log(err);
-});
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
 
 /* ===================================== */
 /* HOME ROUTE */
@@ -329,8 +325,10 @@ app.delete("/billing/:id", async (req, res) => {
 /* SERVER */
 /* ===================================== */
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+
+console.log(`Server running on port ${PORT}`);
+
 });
